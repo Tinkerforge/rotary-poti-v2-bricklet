b84393039abef0670aab1c827b50bacaa187cec3
Olaf Lüke <olaf@tinkerforge.com>
2019-02-19 16:00:15 +0100

    Add API/driver for Rotary Poti Bricklet 2.0

--- software/src/poti.h
+++ software/src/poti.h
@@ -1,7 +1,7 @@
 /* rotary-poti-v2-bricklet
- * Copyright (C) 2018 Olaf Lüke <olaf@tinkerforge.com>
+ * Copyright (C) 2019 Olaf Lüke <olaf@tinkerforge.com>
  *
- * main.c: Initialization for Rotary Poti Bricklet 2.0
+ * poti.h: Potentiometer driver
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,27 +19,14 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#include <stdio.h>
-#include <stdbool.h>
+#ifndef POTI_H
+#define POTI_H
 
-#include "configs/config.h"
+#include <stdint.h>
 
-#include "bricklib2/bootloader/bootloader.h"
-#include "bricklib2/hal/system_timer/system_timer.h"
-#include "bricklib2/logging/logging.h"
-#include "communication.h"
-#include "poti.h"
+void poti_init();
+void poti_tick();
 
-int main(void) {
-	logging_init();
-	logd("Start Rotary Poti Bricklet 2.0\n\r");
+int16_t poti_get_value(void);
 
-	communication_init();
-	poti_init();
-
-	while(true) {
-		bootloader_tick();
-		communication_tick();
-		poti_tick();
-	}
-}
+#endif
