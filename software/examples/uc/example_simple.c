683dc3b55c2101515010b72d71a3bf68659f6296
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:54:39 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_rotary_poti_v2.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your Rotary Poti Bricklet 2.0
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_RotaryPotiV2 rp;
 
